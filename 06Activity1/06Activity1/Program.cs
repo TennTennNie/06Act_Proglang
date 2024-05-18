7eeaf246e33b8fd8b9372c8f42079741252abf38
TennTennNie <144678351+TennTennNie@users.noreply.github.com>
2024-05-18 15:25:01 +0800

    Add files via upload

---
+++ 06Activity1/06Activity1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+    class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                int num1, num2, lastnum, mod;
+
+                Console.WriteLine("Enter Six Digit Numbers: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                num2 = num1 / 10;
+                lastnum = (num1 % 10);
+                mod = (num2 % 7);
+
+                if (lastnum == mod)
+                {
+                    Console.WriteLine("True\n");
+                }
+                else
+                {
+                    Console.WriteLine("False\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
